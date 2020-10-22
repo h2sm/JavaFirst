3a1f72e7b51fe8388574d4a14db894d4f2925b92
h2sm <68076283+h2sm@users.noreply.github.com>
2020-10-22 11:33:50 +0300

    Create second.java

---
+++ second.java
@@ -0,0 +1,47 @@
+public class second {
+    public static void main(String[] args) {
+
+        Complex x = new Complex(2, 3);
+        Complex y = new Complex(4, -2);
+
+        System.out.println("Sum is " + Complex.toSum(x,y));
+        System.out.println("Subtract is "+ Complex.toSubtract(x,y));
+        System.out.println("Multiply is " + Complex.toMultiply(x,y));
+        System.out.println("Divide is " + Complex.toDivide(x,y));
+        }
+}
+class Complex{
+    private double real;
+    private double imaginary;
+    public Complex(double real, double imaginary) {
+       this.real = real;
+       this.imaginary = imaginary;
+    }
+    //не статические(a.+b)
+    public static Complex toSum(Complex x, Complex y) {
+        double realPart = x.real + y.real;
+        double imaginaryPart = x.imaginary + y.imaginary;
+        return new Complex(realPart, imaginaryPart);
+    }
+    public static Complex toSubtract(Complex x, Complex y){
+        return new Complex(x.real - y.real, x.imaginary - y.imaginary);
+    }
+    public static Complex toMultiply(Complex x, Complex y){
+        return new Complex(x.real * y.real, x.imaginary * y.imaginary);
+    }
+    public static Complex toDivide(Complex x, Complex y){
+        Complex num = new Complex(y.real, (-1)* y.imaginary);
+        num = Complex.toMultiply(x, num);
+        double znamenatel = y.real * y.real + y.imaginary * y.imaginary;
+        return new Complex(num.real/znamenatel, num.imaginary/znamenatel);
+    }
+    @Override
+    public String toString() {
+        String realString = this.real+"";
+        String imaginareString = "";
+        if(this.imaginary < 0) imaginareString = this.imaginary+"i";
+        else
+            imaginareString = "+"+this.imaginary+"i";
+        return realString+imaginareString;
+    }
+}
