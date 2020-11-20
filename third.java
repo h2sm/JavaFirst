c751163016f73ca57bc50c8ca731986b3e6765e1
h2sm <68076283+h2sm@users.noreply.github.com>
2020-11-20 14:30:41 +0300

    Update third.java

--- third.java
+++ third.java
@@ -10,15 +10,15 @@ public class third {
 class BankManagement{
     Bank mainBank = new Bank();
     void loadClients(){//загружаем изначальный массив клиентов
-        mainBank.addClient("Евгений Бланов (Е.Бланов)");
+        mainBank.addClient("Евгений Бланов");
         mainBank.addClient("Сосиска Наталья Витальевна");
-        mainBank.addClient("Вагина Ирина");
+        mainBank.addClient("Борис Олегович Ганалов");
         mainBank.addClient("Шмаль Анна Николаевна");
-        mainBank.addClient("Уланов Евгений Бок (У.Е.Бок)");
+        mainBank.addClient("Уланов Евгений Бок");
         mainBank.addClient("Магомедов Зорро");
         mainBank.addClient("Россия Беспутина");
         mainBank.addClient("Казимир Кукуруза");
-        mainBank.addClient("Василий Ебание");
+        mainBank.addClient("Василий ");
 //        mainBank.showMoneyOnAllDeps(0 );
 //        mainBank.addMoneyToDeposit(0, 0, 100);
 //        mainBank.withdrawMoneyFromDeposit(0,2,200);
@@ -26,31 +26,33 @@ class BankManagement{
         //mainBank.deleteOneDeposit(0, 1);
     }
     void test(){
+        Random x = new Random();
         for (int i=0; i<100;i++){
-            Random x = new Random();
-            int y = x.nextInt(100);
-            switch (y){
-                case '0': mainBank.addMoneyToDeposit(0,0,400);
+            //запроить список клиентов -> генерировать рандомный элемент с ограничением
+            switch (x.nextInt(8)){ //новый свитч сделай
+                //вместо sum передавать массив с банкнотами(генерировать случайный moneystack)
+
+                case 0: mainBank.addMoneyToDeposit(0,0,400);//выводим сообщения здесь!!!!!!
                     break;
-                case '1':
+                case 1:
                     mainBank.addMoneyToDeposit(0,1,300);
                     break;
-                case '2':
+                case 2:
                     mainBank.addMoneyToDeposit(0,1,350);
                     break;
-                case '3':
+                case 3:
                     mainBank.addMoneyToDeposit(0,2,400);
                     break;
-                case '4':
+                case 4:
                     mainBank.deleteClient(0);
                     break;
-                case '5':
+                case 5:
                     mainBank.deleteOneDeposit(0,2);
                     break;
-                case '6':
+                case 6:
                     mainBank.openNewDeposit(0);
                     break;
-                case '7':
+                case 7:
                     mainBank.addMonthlyRate(0);
                     break;
             }
@@ -58,8 +60,82 @@ class BankManagement{
     }
 }
 class Bank{
-    ArrayList<String> listOfClients = new ArrayList<>();//список клиентов
-    ArrayList<Client> clients = new ArrayList<>();//массив клиентов и их депозитов
+    private ArrayList<String> listOfClients = new ArrayList<>();//список клиентов
+    //private ArrayList<Client> clients = new ArrayList<>();//массив клиентов и их депозитов
+    final MoneyStack bankCapit = new MoneyStack(new int[] {100,100,100,100,100,100,100});
+    Map<String, Client> map = new Map<String, Client>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public Client get(Object key) {
+            return null;
+        }
+
+        @Override
+        public Client put(String key, Client value) {
+            return null;
+        }
+
+        @Override
+        public Client remove(Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(Map<? extends String, ? extends Client> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Set<String> keySet() {
+            return null;
+        }
+
+        @Override
+        public Collection<Client> values() {
+            return null;
+        }
+
+        @Override
+        public Set<Entry<String, Client>> entrySet() {
+            return null;
+        }
+    };
+
+    public ArrayList<String> getListOfClients() {
+        return listOfClients;
+    }
+
+    public void setListOfClients(ArrayList<String> listOfClients) {
+        this.listOfClients = listOfClients;
+    }
+
+    public MoneyStack getBankCapit() {
+        return bankCapit;
+    }
 
     void addClient(String nameOfClient){//добавление клиента
         if(listOfClients.contains(nameOfClient)){//проверка на повторное добавление
@@ -67,11 +143,11 @@ class Bank{
         }
         else{
             listOfClients.add(nameOfClient);//добавляем клиента если его не было
-            clients.add(new Client(nameOfClient));//создаем инстанс клииента с его депозитами
+            map.put(nameOfClient, new Client(nameOfClient));
             System.out.println(nameOfClient + " добавлен");
         }
     }
-    void deleteClient(Integer ID){//процедура удаления клиента
+    void deleteClient(Integer ID){//процедура удаления клиента + ПЕРЕДАВАЙ Client
         //showMoneyOnAllDeps(ID); //для быстрой проверки
         String name = listOfClients.get(ID);//имя клиента
         Client x = clients.get(listOfClients.indexOf(name));//достаем клиента
@@ -110,35 +186,28 @@ class Bank{
         Client x = clients.get(ID);
         x.monthRate();
     }
-    //разместить массив клиентов
-    //добавление + удаление
 }
 class Client{
-    ArrayList<Integer> deposits = new ArrayList<>();
-    String clientName;
-    int rate = 5; //ставка - 5 процентных пунктов
-    double bankCapit = 100000.0;//запас банкнот банка
+    private final ArrayList<Integer> deposits = new ArrayList<>();//все поля защищенные + сеттер и геттер (дописать)
+    private final String clientName;
+    private final int rate = 5; //ставка - 5 процентных пунктов
     Client(String name){
         this.clientName = name;
     }
-    void addSumOnDeposit(Integer sum, Integer numOfDeposit) throws IndexOutOfBoundsException{//добавить сумму на определенный депозит
-        try {
-            if (deposits.isEmpty()) {// НЕ ЛЕЗЬ РАБОТАЕТ !!!!!!!!!
-                capitalAdd(sum);
-                deposits.add(numOfDeposit, sum);
-                //bankCapit+=sum;
+    void addSumOnDeposit(Integer sum, Integer numOfDeposit) {//добавить сумму на определенный депозит
+        if (deposits.size() < numOfDeposit){//если зачисление на счет, который еще не открыли
+            System.out.println("Зачисление невозможно, депозит " + numOfDeposit + " еще не открыт" );
+        }
+        else if (deposits.isEmpty()) {
+            capitalAdd(sum);//добавляем средства банку
+            deposits.add(sum);//добавляем денег клиенту
             }
-            else{
-                deposits.set(numOfDeposit, sum + showSumOnOneDeposit(numOfDeposit));
+        else{
+            deposits.set(numOfDeposit, sum + showSumOnOneDeposit(numOfDeposit));
                 //bankCapit+=sum;
                 capitalAdd(sum);
             }
         }
-        catch (Exception exception){
-            deposits.add(numOfDeposit,sum);
-            bankCapit+=sum;
-        }
-    }
     Integer getCountOfDeposits(){//количество депозитов клиента
         return deposits.size();
     }
@@ -147,19 +216,20 @@ class Client{
         return deposits;
     }
     Integer showSumOnOneDeposit(Integer numOfDeposit){//сумма денег на определенном депозите
-        if (deposits.size() < numOfDeposit) {
+        if (deposits.size() <= numOfDeposit) {
             return 0;
         }
         else {
             return deposits.get(numOfDeposit);
         }
     }
-    void withdrawMoneyFromDeposit(Integer sum, Integer numOfDeposit){//снять сумму с определенного депозита
+    void withdrawMoneyFromDeposit(int sum, int numOfDeposit) {//снять сумму с определенного депозита
         if ((showSumOnOneDeposit(numOfDeposit) - sum ) > 0 && capitalWithdraw(sum)) {//если денег для снятия достаточно как у банка, так и у клиента
             deposits.set(numOfDeposit, showSumOnOneDeposit(numOfDeposit) - sum);
+
         }
         else{//если недостаточно
-            System.out.println("Недостаточно средств для списания");
+            System.out.println("Недостаточно средств для списания");//взаимодействие консоли только через main, не в методах
         }
     }
     void addDeposit(){//открытие нового депозита
@@ -196,7 +266,48 @@ class Client{
             x+=y;
             deposits.set(i,x);
         }
+        System.out.println("да");
     }
 
 
+}
+class MoneyStack{//когда клиент вносит деньги - вызываем класс
+    public static final int COUNT = 7;//капсом пишут константы
+    private static final int NOMINALS[] = {1, 5, 10, 50, 100, 500, 1000};//номиналы
+    private int amounts[] = new int[COUNT];//количество банкнот
+    MoneyStack(int[] amounts){
+        this.amounts = amounts;
+    }
+
+    //когда клиент забирает  - выделяет подпачки банкнот (выделять %1000, %500, %100)
+    //генерировать исключения NotEnoughBanknotsException - обрабатывать исключение в switch
+    //массив из у.е.
+    MoneyStack selectMoneyStackBy(int sum) throws NotEnoughBanknotsException {//выбрать стек на сумму
+        int newAmounts[] = new int[COUNT];
+        for (int i=COUNT-1; i>=0;i--){
+            while (sum>=NOMINALS[i] && amounts[i]>0){
+                newAmounts[i]++;
+                amounts[i]--;
+                sum-=NOMINALS[i];
+            }
+        }
+       if (sum==0){
+           return new MoneyStack(newAmounts);
+       }
+       else {
+           throw new NotEnoughBanknotsException("Денег нет, но вы держитесь");
+       }
+    }
+}
+class BankException extends Exception{
+    //исключения наследуются через этот класс
+    BankException(String msg){
+        super(msg);
+    }
+    BankException(){}
+}
+class NotEnoughBanknotsException extends BankException{
+    NotEnoughBanknotsException(String msg){
+        super(msg);
+    }
 }
